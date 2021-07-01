7eb69756a038f2e02187d46499f673f491ead9b1
AxelGard <axel.gard@protonmail.com>
2021-07-01 17:54:23 +0200

    update version nr

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='inkpot',
-    version='0.1.0',
+    version='0.1.1',
     description='a small simple library for generating documentation from docstrings',
     url='https://github.com/AxelGard/inkpot',
     author='Axel Gard',
