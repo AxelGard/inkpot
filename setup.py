c46b33ad63bc4cbc4384682f0f9d3b109d348bab
AxelGard <axel.gard@protonmail.com>
2021-07-05 17:55:43 +0200

    added dep tabulate in setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    install_requires=[],
+    install_requires=["tabulate"],
 
 
     classifiers=[
