cf360bd1fb2ce68d68b7d5f80cc8ee14991a9968
ViktorBusk <bviktor228@gmail.com>
2021-07-07 08:44:15 +0200

    Ouput seems to be working as expected

--- inkpot/file.py
+++ inkpot/file.py
@@ -3,9 +3,7 @@ class instance for one file
 """
 
 import ast
-
 from os.path import basename, splitext
-from tabulate import tabulate
 
 
 class File:
@@ -13,13 +11,15 @@ class File:
 
     NODE_TYPES = {ast.ClassDef: 'class',
                   ast.FunctionDef: 'def',
-                  ast.AsyncFunctionDef: 'async-def'
+                  # ast.AsyncFunctionDef: 'async-def'
                   }
 
     def __init__(self, path: str):
         """ constructor """
         self._path = path
         self.nodes = {}
+        for type_ in File.NODE_TYPES.values():
+            self.nodes[type_] = {}
 
     def filter(self, module: str = '<string>'):
         """ find node-types and docstrings in the file """
@@ -34,42 +34,71 @@ class File:
                 for node in ast.walk(tree):
                     # Classes
                     if isinstance(node, ast.ClassDef):
-                        # FIXME: Maybe add inheritance support: node.bases[0].id
+                        node.name = ast.unparse(node).split('\n')[0][6:-1]
+                        node.__doc__ = ast.get_docstring(node)
+
+                        new_body = []
                         for child in node.body:
                             # Class methods
-                            if isinstance(child, ast.FunctionDef) or \
-                                    isinstance(child, ast.AsyncFunctionDef):
+                            if isinstance(child, ast.FunctionDef):
                                 child.parent = node
+                                new_body.append(child)
                             # Nested class support
                             if isinstance(child, ast.ClassDef):
                                 child.parent = node
+                                new_body.append(child)
+
+                        node.body = new_body
                     # Functions
-                    if isinstance(node, ast.FunctionDef) or \
-                            isinstance(node, ast.AsyncFunctionDef):
+                    if isinstance(node, ast.FunctionDef):
+                        node.__doc__ = ast.get_docstring(node)
+                        node.name = ast.unparse(node).split('\n')[0][4:-1]
+
+                        new_body = []
                         for child in node.body:
                             # Nested function support
                             if isinstance(child, ast.FunctionDef):
                                 child.parent = node
+                                new_body.append(child)
+
+                        node.body = new_body
 
                     if isinstance(node, tuple(File.NODE_TYPES)):
-                        if hasattr(node, 'name'):
-                            self.nodes[str(node.name)] = node
+                        # Only add root nodes with a name
+                        if hasattr(node, 'name') and not hasattr(node, 'parent'):
+                            type_ = File.NODE_TYPES.get(type(node))
+                            self.nodes[type_][str(node.name)] = node
+
+    def _recursive_tree(self, node, layer=0):
+        type_ = File.NODE_TYPES.get(type(node))
+        header = (type_ + " " + node.name).replace("*",
+                                                   "\*").replace("__", "\_\_")
+        docstring = '`'+str(node.__doc__)+'`'
+
+        if type_ == 'class':
+            header = '### ' + header
+        elif type_ == 'def':
+            header = "**" + header + "** \\"
+
+        if layer > 0:
+            print(">" * layer, header)
+            print(">" * layer, docstring)
+            print(">" * layer)
+        else:
+            print(header)
+            print(docstring, end="\n\n")
+
+        if len(node.body) > 0:
+            for child in node.body:
+                self._recursive_tree(child, layer+1)
 
     def output(self):
         """ outputs all node-types and their respective docstrings """
 
-        head = ["type", "name", "doc-str"]
-        table = []
-
         # Use pipe for markdown files
         print("## %s" % (self._path))
-        # print(tabulate(table, headers=head, tablefmt="pipe"))
 
-        for node in self.nodes.values():
-            print('Type:', File.NODE_TYPES.get(type(node)))
-            print('Name:', node.name)
-            if hasattr(node, 'parent'):
-                print('Parent:', node.parent.name)
-            print(ast.get_docstring(node))
-            print('')
+        for type_ in self.nodes:
+            for root_node in self.nodes[type_].values():
+                self._recursive_tree(root_node)
         print('')
