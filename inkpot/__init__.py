e02706c09ad0f821252006f089d71030912f74ad
Axel Gard <axel.gard@protonmail.com>
2024-06-09 09:16:00 +0200

    new auth for pypi

--- inkpot/__init__.py
+++ inkpot/__init__.py
@@ -2,4 +2,4 @@ from . import directory
 from . import config 
 from . import file 
 
-__version__ = "2.2.4"
+__version__ = "2.2.5"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='inkpot',
-    version='2.2.4',
+    version='2.2.5',
     description='a small simple library for generating documentation from docstrings',
     url='https://github.com/AxelGard/inkpot',
     author='Axel Gard',
